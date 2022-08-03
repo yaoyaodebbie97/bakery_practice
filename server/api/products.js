4077e61771d517603a219f023f51977a8c264ee2
Kandance Ferguson <kandance.ferguson@gmail.com>
2022-08-03 14:13:12 -0400

    updated product routes with allproducts, products by category, single product

---
+++ server/api/products.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const {
+  models: { Product },
+} = require('../db');
+
+//ROUTE /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll();
+    res.json(allProducts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:category', async (req, res, next) => {
+  try {
+    const allProducts = await Product.findAll({
+      where: {
+        category: req.params.category,
+      },
+    });
+    res.json(allProducts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
