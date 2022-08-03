30dd47a2f9d060c6f212d8265a80b5c63cbbead2
Kristin Levine <kristinjlevine@gmail.com>
2022-08-03 08:55:59 -0500

    userRoutes

---
+++ server/api/middleware.js
@@ -0,0 +1,25 @@
+const { models: { User }} = require('../db')
+
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+    const user = await User.findByToken(token)
+    req.user = user
+    next()
+  } catch(err) {
+    next(err)
+  }
+}
+
+const isAdmin = (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return res.status(403).send('Unauthorized User')
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  requireToken,
+  isAdmin
+}
--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,41 @@
 const router = require('express').Router()
-const { models: { User }} = require('../db')
+const { models: { User, Order }} = require('../db')
+const { requireToken, isAdmin } = require('./middleware')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+// get all users if logged in & admin
+router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
+      attributes: ['id', 'email']
     })
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+
+// create new user
+router.post(`/`, async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body)
+    res.send(newUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// get single user and order history
+router.get('/:userId', requireToken, async (req, res, next) => {
+  try {
+    const user = await User.findByPK(req.params.id, {
+      include: [Order]
+    })
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
