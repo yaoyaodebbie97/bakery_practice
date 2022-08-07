bd8f7a57edf339d29784546507c60f10b01963be
Kandance Ferguson <kandance.ferguson@gmail.com>
2022-08-07 15:12:58 -0400

    working on nav bar css

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,44 +1,41 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+  const { username } = props;
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
-      <img src=""/>
+    <div id='content'>
+      {/* <h3>Welcome, {username}</h3> */}
+      <img src='' />
       <div>
         <h3>About Us</h3>
         <p>Established in 2022.</p>
       </div>
       <div>
-        <Link to='/products/category/bread'>Bread    </Link>
-        <Link to='/products/category/cupcakes'>Cupcakes    </Link>
-        <Link to='/products/category/cookies'>Cookies    </Link>
-        <Link to='/products/category/pies'>Pies    </Link>
-        <Link to='/products/category/pastries'>Pastries    </Link>
-        <Link to='/products/category/muffins'>Muffins    </Link>
-        <Link to='/products/category/cakes'>Cakes    </Link>
-        </div> 
+        <Link to='/products/category/bread'>Bread </Link>
+        <Link to='/products/category/cupcakes'>Cupcakes </Link>
+        <Link to='/products/category/cookies'>Cookies </Link>
+        <Link to='/products/category/pies'>Pies </Link>
+        <Link to='/products/category/pastries'>Pastries </Link>
+        <Link to='/products/category/muffins'>Muffins </Link>
+        <Link to='/products/category/cakes'>Cakes </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
-
-
-
+export default connect(mapState)(Home);
