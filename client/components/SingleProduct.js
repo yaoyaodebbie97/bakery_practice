69fc817eebc2973a1d3a1f11e9fd9cb03d8c56cd
Yao Guo <yaoyaodebbie@gmail.com>
2022-08-09 09:02:19 -0700

    adding loading message

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -30,34 +30,45 @@ class SingleProduct extends Component {
         const product = this.props.product
         return (
         <div className={"singleProductContainer"}>
-            <img className={"responsive"} src = {product.imageUrl} ></img>
-            <div className={"singleProduct"}>
-              <div className={"singleHeader"} >
-            <p> {product.productName} </p>
+            {isNaN(product.price/100)
+            ?  <>
+                <h1 className='align-items-center'> Still Loading </h1>
+                <img
+                  src={
+                    'https://i.pinimg.com/originals/da/08/2c/da082c176aea0f8e5ab294ff7a0f90d1.gif'
+                  }
+                />
+              </>
+            :
+            <div> 
+              <img className={"responsive"} src = {product.imageUrl} ></img>
+              <div className={"singleProduct"}>
+                <div className={"singleHeader"} >
+              <p> {product.productName} </p>
+              </div>
+              <p> {product.description}</p>
+              <p>${(product.price / 100).toFixed(2)}</p>
+
+              <select className={'quantity'} name="quantity" id="quantity" onChange = {this.handleSelect} >
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                  <option value={6}>6</option>
+                  <option value={7}>7</option>
+                  <option value={8}>8</option>
+                  <option value={9}>9</option>
+                  <option value={10}>10</option>
+              </select>
+              <button
+                  className={'addToCartButton'}
+                  onClick={() => this.handleAdd()}>
+                      Add to Cart
+              </button>
+              </div>
             </div>
-            <p> {product.description}</p>
-            <p>${(product.price / 100).toFixed(2)}</p>
-
-            <select className={'quantity'} name="quantity" id="quantity" onChange = {this.handleSelect} >
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
-                <option value={10}>10</option>
-            </select>
-
-            <button
-                className={'addToCartButton'}
-                onClick={() => this.handleAdd()}>
-                    Add to Cart
-            </button>
-            </div>
-
+    }
         </div>
         )
   }
